cf6b659502d9c018259d39a5a27be304eac33e54
Luther Calvin Riggs <luthercalvinriggsllc@gmail.com>
2020-07-23 15:35:51 -0400

    updated js-notes and js-code-library

--- JavaScript/js-code-library.js
+++ JavaScript/js-code-library.js
@@ -1,55 +1,297 @@
 // Arrays
 
-    // Accessing first array element
-    let first = arrayName[0];
+    // Accessing FIRST array element
+        let first = arrayName[0];
 
-    // Accessing last array element 
-    let last = arrayName[arrayName.length - 1];
+    // Accessing LAST array element 
+        let last = arrayName[arrayName.length - 1];
+
+    // Converts an array to a comma separated string
+        arrayName.toString();
+
+    // Join all array elements into a string, also allows you to specify a separator
+        arrayName.join(" * ");
+
+    // Returns the LAST element from an array
+        let x = arrayName.pop();
+
+    // Returns the new array length after adding a value
+        let x = arrayName.push("newElement");
+
+    // Returns the new array length
+        arrayName.unshift("newElement");
+
+    // Returns the string that was "shifted out"
+        let x = arrayName.shift();
+
+    // Adds a new element to an array (AT THE END)
+        arrayName.push("newElement");
+    
+    // Adds a new element to an array (AT THE BEGINNING) and "unshifts" older elements
+        arrayName.unshift("newElement");
+
+    // Appends a new element to an array
+        arrayName[arrayName.length] = "newElement";
+
+    // Adds new items to an array
+        arrayName.splice(x, y, "newElement1", "newElement2");
+        // The first parameter (x) defines the position where new elelements should be ADDED (spliced in). The second parameter (y) defines how many elements should be REMOVED). The rest of the parameters ("newElement1", "newElement2") define the new elemennts to be added.
+
+    // Removes the LAST element from an array
+        arrayName.pop();
+
+    // Removes the FIRST array element and "shift" all other elements to a lower index
+        arrayName.shift();
+
+    // Deletes an element from an array
+        delete arrayName[x];
+        // * Using `delete` may leave undefined holes in the array. You should use `pop()` or `shift()` instead.
+
+    // Cuts out a piece of an array into a new array, starting at 'index-number'
+        let newArray = arrayName.slice(index-number);
+        // * The `slice()` method creates a new array. It does not remove any elements from the source array
+
+    // Cuts out a piece of an array into a new array from the starting argument up to, but not including, the end argument 
+        let newArray = arrayName.slice(start-argument, end-argument);
+        // If the end argument is omitted, slice() will cut out the rest of the array
+
+    // Removes elements without leaving "holes"
+        arrayName.splice(0, 1);
+        arrayName.splice(x, y);
+        // The first parameter (x) defines the position where new elements should be ADDED (spliced in). The second parameter (y) defines how many elemennts should be REMOVED. The rest of the parameters are omitted. No new elemennts will be added.
+
+    // Creates a new array by merging (concatenating) existing arrays
+        let newArray = arrayName1.concat(arrayName2);
+        let newArray = arrayName1.concat(arrayName2, arrayName3, ..., arrayNameN);
+        // * The `concat()` method does not change the existing arrays. It always returns a new array.
+
+    // Creates a new array my concatenating an existing array with a new string
+        let newArray = arrayName1.concat("string");
+
+    // Splits the contents of an array using a designator seperator
+        arrayName[x].split(':');
+        let newArrayName = arrayName[x].split(':');
+        // This creates a new array with new elements based on what was split. To recall the items that were split, use newArrayName[0] for the elemennt before the separator and newArrayName[n] for the element after the separator
+
+    // LIFO & FILO - adds to end, pulls from end
+        arrayName.push("newElement")
+        arrayName.pop();    
+
+    // FIFO & LILO - adds to end, pulls from beginning
+        arrayName.push("newElement");
+        arrayName.shift();
+
+    // Rotates front to back
+        arrayName.unshift();
+        arrayName.pop()
+
+    // Rotates back to front
+        arrayName.shift();
+        arrayName.push();
+
+
 
     // Using a `for` loop on an array
-    let fruit, text, fLen, i;
-    let fruits = ["Banana", "Orange", "Apple", "Mango"];    
-    fLen = fruits.length;
+        let fruit, text, fLen, i;
+        let fruits = ["Banana", "Orange", "Apple", "Mango"];    
+        fLen = fruits.length;
 
-    text = "<ul>";
-        for (i = 0; i < fLen; i++) {
-            text += "<li>" + fruits[i] + "</li>";
-        }
-    text += "</ul>";
+        text = "<ul>";
+            for (i = 0; i < fLen; i++) {
+                text += "<li>" + fruits[i] + "</li>";
+            }
+        text += "</ul>";
 
 
     // Using `forEach()` on an array
-    let fruit, text;
-    let fruits = ["Banana", "Orange", "Apple", "Mango"];    
+        let fruit, text;
+        let fruits = ["Banana", "Orange", "Apple", "Mango"];    
 
-    text = "<ul>";
-        fruits.forEach(myFunction);
-    text += "</ul>";
+        text = "<ul>";
+            fruits.forEach(myFunction);
+        text += "</ul>";
 
-    function myFunction(value) {
-        text += "<li>" + value + "</li>";
-    }
-
-    // Adding an element to an array using `push()`
-    arrayName.push('newElementName');
-
-    // Adding an element to an array using `length`
-    arrayName[arrayName.length] = "newElementName";
+        function myFunction(value) {
+            text += "<li>" + value + "</li>";
+        }
 
     // To test for an array 
-    Array.isArray( arrayName );   // returns true - not supported in older browsers
+        Array.isArray( arrayName );   // returns true - not supported in older browsers
 
-        // or
+            // or
 
-    function isArray(x) {
-        return x.constructor.toString().indexOf("Array") > -1;
-    }           // Always returns true if the object prototype contains the word "Array"
+        function isArray(x) {
+            return x.constructor.toString().indexOf("Array") > -1;
+        }           // Always returns true if the object prototype contains the word "Array"
 
-        // or
+            // or
 
-    arrayName instanceof Array;   // returns true if an object is created by a given constructor
+        arrayName instanceof Array;   // returns true if an object is created by a given constructor
 
     // 
+
+
+
+
+
+// Loops 
+
+    // for Loop
+    for (initializer; condition; final-expression) {
+        // code to run
+    }
+
+    // for Loop on an array
+        for (let i = 0; i < arrayName.length; i++) {
+            if (i === arrayName.length - 1) {
+                info += 'and ' + arrayName[i] + '.';
+            } else {
+                info += arrayName[i] + ', ';
+            }
+        }
+
+    // * With any for loop, be sure to increment or decrement the initializer or the for loop will run infinitely. 
+
+    // When using an `if` fuction in a loop, and the condition is met, you can exit a for loop by using the keyword `break;`
+        for (let i = 0; i < contacts.length; i++) {
+            let splitContact = contacts[i].split(':');
+            if (splitContact[0].toLowerCase() === searchName) {
+                para.textContent = splitContact[0] + '\'s number is ' + splitContact[1] + '.';
+                break;
+            } else {
+                para.textContent = 'Contact not found.';
+            }
+        };
+
+    // When using an `if` function in a loop, and the condition is not yet met, you can use the `continue;` keyword to keep the loop going
+        for (let i = 1; i <= num; i++) {
+            let sqRoot = Math.sqrt(i);
+            if (Math.floor(sqRoot) !== sqRoot) {
+                continue;
+            }
+
+            para.textContent += i + ' ';
+        }
+
+    // while Loop
+        initializer
+        while (condition) {
+            // code to run
+
+            final-expression
+        }
+
+    // while Loop on an array
+        let i = 0;
+
+        while (i < cats.length) {
+            if (i === cats.length - 1) {
+                info += 'and ' + cats[i] + '.';
+            } else {
+                info += cats[i] + ', ';
+            }
+
+            i++;
+        }
+
+    // do...while Loop
+        initializer
+        do {
+            // code to run
+
+            final-expression
+        } while (condition)
+
+    // do...while Loop on an array
+        let i = 0;
+
+        do {
+            if (i === cats.length - 1) {
+                info += 'and ' + cats[i] + '.';
+            } else {
+                info += cats[i] + ', ';
+            }
+
+            i++;
+        } while (i < cats.length);
+
+    // Countdown Loop using for Loop
+        const output = document.querySelector('.output');
+        output.innerHTML = '';
+        
+        for(i = 10; i >= 0; i--){
+            let para = document.createElement('p');
+            
+            if (i === 10){
+                para.textContent = "T-minus " + i;
+            } else if (i !== 10 && i > 0){
+                para.textContent = i;
+            } else {
+                para.textContent = "BLAST OFF!";
+            }
+
+            output.appendChild(para);
+        }
+    
+    // Countdown Loop using while Loop
+        const output = document.querySelector('.output');
+        output.innerHTML = '';
+
+        let i = 10;
+
+        while(i >= 0) {
+            let para = document.createElement('p');
+            
+            if(i === 10) {
+                para.textContent = 'Countdown ' + i;
+            } else if(i === 0) {
+                para.textContent = 'Blast off!';
+            } else {
+                para.textContent = i;
+            }
+
+            output.appendChild(para);
+
+            i--;
+        }
+
+    // Building a guest list and no-entry list from an array
+        const people = ['Chris', 'Anne', 'Colin', 'Terri', 'Phil', 'Lola', 'Sam', 'Kay', 'Bruce'];
+        
+        const admitted = document.querySelector('.admitted');
+        const refused = document.querySelector('.refused');
+        admitted.textContent = 'Admit: ';
+        refused.textContent = 'Refuse: ';
+
+        let i = 0;
+
+        do {
+            
+            if(people[i].toLowerCase() === "phil" || people[i].toLowerCase() === "lola") {
+                refused.textContent += people[i] + ", ";
+            } else {
+                admitted.textContent += people[i] + ", ";
+            }
+
+            i++;
+
+        } while( i < people.length );
+
+        refused.textContent = refused.textContent.slice(0, refused.textContent.length-2) + ".";
+        admitted.textContent = admitted.textContent.slice(0, admitted.textContent.length-2) + ".";
+
+    // Printing array items to a <li>   ?????
+        let list = document.createElement('ul');
+
+        for (i=0; i < myArray.length; i++){
+            let list = document.createElement('li');
+            list.textContent = myArray[i];
+        }
+
+        const section = document.querySelector('section');
+        section.appendChild(list);
+
+        
 
 
 
@@ -58,17 +300,6 @@
 
     // Capitalize the first letter of a string
     txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-
-
-
-
-// Loops 
-
-    // Using a `for` loop on an array
-        for (i = 0; i < someNumber; i++) {
-            // some code
-        }
-
 
 
 
