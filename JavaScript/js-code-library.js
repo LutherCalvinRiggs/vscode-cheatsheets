8bd75b4be223c07ba6790d782fd6935ca539bae6
Luther Calvin Riggs <luthercalvinriggsllc@gmail.com>
2020-09-30 16:31:34 -0400

    added notes for localStorage and Git Procedures for Pull Requests and Branches

--- JavaScript/js-code-library.js
+++ JavaScript/js-code-library.js
@@ -2956,6 +2956,128 @@ value.isNaN();
 
 
 
+// localStorage / Web Storage API
+    // Provides mechanisms by which brosers cann securely store key/value pairs
+
+    // Basic Concepts
+        // Storage objects are simple key-value stores, similar to objects, but they stay intact through page loads. The keys and the values are always strings (note that, as with objects, integer keys will be automatically converted to strings). You can access these value like an object, or with the `Storage.getItem()` and `Storage.setItem()` methods.
+            // The following three lines all set the same entry
+                localStorage.colorSetting = '#a4509b';
+                localStorage['colorSetting'] = '#a4509b';
+                localStorage.setItem('colorSetting', '#a4509b');
+        // There are two mechanisms within Web Storage
+            // `sessionStorage` maintains a separate storage area for each given origin that's available for the durationn of the page session (as long as the browser is open, including page reloads and restores)
+            // `localStorage` does the same thing, but persists even whenn the browser is closed and reopened
+        // These mechanisms are available via the `Window.sessionStorage` and `Window.localStorage` properties - invoking one of these will create an instance of the Storage object, through which data items can be set, retrieved, and removed. A different Storage object is used for the `sessionStorage` and `localStorage` for each origin. THEY FUNCTION AND ARE CONTROLLED SEPARATELY.
+            // For example, initially calling `localStorage` on a document will return a Storage object; calling `sessionStorage` on a document will returnn a different Storage object. Both of these can be manipulated in the same way, but separately.
+    
+    // Feature-detecting localStorage
+        // To be able to use localStorage, we should first verify that it is supported and available inn the current browsing session. 
+        
+        // Testing for availability
+            // The following is a function that detects whether localStorage is both supported and available:
+                function storageAvailable(type) {
+                    var storage;
+                    try {
+                        storage = window[type];
+                        var x = '__storage_test__';
+                        storage.setItem(x, x);
+                        storage.removeItem(x);
+                        return true;
+                    }
+                    catch(e) {
+                        return e instanceof DOMException && (
+                            // everything except Firefox
+                            e.code === 22 ||
+                            // Firefox
+                            e.code === 1014 ||
+                            // test name field too, because code might not be present
+                            // everything except Firefox
+                            e.name === 'QuotaExceededError' ||
+                            // Firefox
+                            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+                            // acknowledge QuotaExceededError only if there's something already stored
+                            (storage && storage.length !== 0);
+                    }
+                }
+            // The following is how you use the above function to detect localStorage
+                if (storageAvailable('localStorage')) {
+                    // Yippee! We can use localStorage awesomeness
+                }
+                else {
+                    // Too bad, no localStorage for us
+                }
+            // You can test for sessionStorage instead by calling `storageAvailable('sessionStorage')`
+
+    // Example
+        // Testing whether your stage has beenn populated
+            // To start, in main.js, we test whether the storage object has already been populated (ie, the page was previously accessed):
+                if(!localStorage.getItem('bgcolor')) {
+                    populateStorage();
+                } else {
+                    setStyles();
+                }
+            // The Storage.getItem() methods is used to get a data item from storage; in this case, we are testing to see whether the `bgcolor` item exists; if not, we run `populateStorage()` to add the existing customization values to the storage. If there are already values there, we run `setStyles()` to update the page styling with the stored values.
+            // NOTE: You could also use Storage.length to test whether the storage object is empty or not.
+        
+        // Getting values from storage
+            // As noted above, values can be retrieved from storage using Storage.getItem(). This takes the key of the data item as an argument, and returns the data value. 
+                function setStyles() {
+                    var currentColor = localStorage.getItem('bgcolor');
+                    var currentFont = localStorage.getItem('font');
+                    var currentImage = localStorage.getItem('image');
+                
+                    document.getElementById('bgcolor').value = currentColor;
+                    document.getElementById('font').value = currentFont;
+                    document.getElementById('image').value = currentImage;
+                
+                    htmlElem.style.backgroundColor = '#' + currentColor;
+                    pElem.style.fontFamily = currentFont;
+                    imgElem.setAttribute('src', currentImage);
+                }
+            // Here, the first three lines grab the values from local storage. Next, we set the values displayed in the form elements to those values, so that they keep in sync when you reload the page. Finally, we update the styles/decorative image on the page, so your customization options come up again on reload.
+        
+        // Setting values in storage
+            // Storage.setItem() is used both to create new data items, and (if the data item already exists) update exisiting values. This takes two arguments - the key of the data item to create/modify, and the value to store in it.
+                function populateStorage() {
+                    localStorage.setItem('bgcolor', document.getElementById('bgcolor').value);
+                    localStorage.setItem('font', document.getElementById('font').value);
+                    localStorage.setItem('image', document.getElementById('image').value);
+                
+                    setStyles();
+                }
+            // The populateStorage() function nsets three items in local storage - the background color, font, and image path. It then runs the setStyles() function nto update the page styles, etc. 
+            // We've also included an `onchange` handler onn each form element so that the data and styling are updated whenever a form value is changed.
+                bgcolorForm.onchange = populateStorage;
+                fontForm.onchange = populateStorage;
+                imageForm.onchange = populateStorage;
+        
+        // Responding to storage changes with the StorageEvent
+            // The StorageEvent is fired whenever a change is made to the Storage object (note that this event is not fired for sessionStorage changes). This won't work on the same page that is making the changes - it is really a way for other pages on the domain using the storage to sync any changes that are made. Pages on other domains can't access the same storage objects.
+            // On the events page (see events.js) the only JavaScript is as follows:
+                window.addEventListener('storage', function(e) {  
+                    document.querySelector('.my-key').textContent = e.key;
+                    document.querySelector('.my-old').textContent = e.oldValue;
+                    document.querySelector('.my-new').textContent = e.newValue;
+                    document.querySelector('.my-url').textContent = e.url;
+                    document.querySelector('.my-storage').textContent = JSON.stringify(e.storageArea);
+                });
+            // Here we add an event listener to the `window` object that fires when the Storage object associated with the current origin is changed. As you can see above, the event object associated with this event has a number of properties containing useful information - the key of the data that changed, the old value before the change, the new value after that change, the URL of the document that changed the storage, and the storage object itself (which we've stringified so you can see its content). 
+        
+        // Deleting data records
+            // Web Storage also provides a couple of simple methods to remove data. We don't use these in our demo, but they are very simple to add to your project
+                Storage.removeItem() // takes a simgle argument - the key of the data item you want to remove - and removes it from the storage object for that domain
+                Storage.clear() // takes no arguments, and simply empties the entire storage object for that domain
+
+
+
+
+
+
+
+
+
+
 
 // Applied Coding
     
